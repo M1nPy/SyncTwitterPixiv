31f2fb043d768dbbd3a142fc030a12f52f4648d8
M1nPy <kuriputon224@yahoo.co.jp>
2020-08-09 01:03:17 +0900

    pixivlink convert to csv

--- twpx.py
+++ twpx.py
@@ -4,6 +4,7 @@ import sys
 import re
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 #正規表現
 rege_pixiv = r'.*pixiv.*'
@@ -23,6 +24,7 @@ api = tweepy.API(auth)
 #フォロー取得
 hataraku_data = api.friends_ids("M1nPy")
 
+list_pixivlinks=[]
 #pixiv取得
 for following in hataraku_data:
     status_date=api.get_user(following)
@@ -32,15 +34,19 @@ for following in hataraku_data:
     except:
         status_url=''
     if re.match(rege_pixiv,status_url) != None:
-        print(status_url)
+        list_pixivlinks.append((status_date.name,status_url))
     #プロフィール欄
     else:
         status_des=status_date.entities["description"]["urls"]
         for des_url in status_des:
             status_reurl=des_url["expanded_url"]
             if re.match(rege_pixiv,status_reurl) != None:
-                print(status_reurl)
+                list_pixivlinks.append((status_date.name,status_reurl))
                 break
         else:
-            print(status_date.name)
+            list_pixivlinks.append((status_date.name,None))
     sleep(1)
+
+#データフレームに変換してcsv出力
+df_pixivlinks=pd.DataFrame(list_pixivlinks)
+df_pixivlinks.to_csv(os.getcwd()+'/pixivlink.csv')
