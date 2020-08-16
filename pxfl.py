a31b748d9fe273335257dfbb9c1add22b109b1ed
M1nPy <sundayoblivion@gmail.com>
2020-08-16 16:38:38 +0900

    syntax fixed

--- pxfl.py
+++ pxfl.py
@@ -8,35 +8,35 @@ from time import sleep
 import pandas as pd
 
 
-#環境変数ロード
+# 環境変数ロード
 load_dotenv()
 
-#csv読み込み
-df_pixivlinks=pd.read_csv(os.getcwd()+'/pixivlink.csv')
+# csv読み込み
+df_pixivlinks = pd.read_csv(os.getcwd()+'/pixivlink.csv')
 
-option=webdriver.ChromeOptions()
+option = webdriver.ChromeOptions()
 option.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36')
-#chormedriverのpath
-driver_path="SyncTwitterPixiv/chromedriver.exe"
+# chormedriverのpath
+driver_path = "SyncTwitterPixiv/chromedriver.exe"
 
-driver=webdriver.Chrome(executable_path=driver_path,options=option)
+driver = webdriver.Chrome(executable_path=driver_path, options=option)
 
 driver.get("https://accounts.pixiv.net/login")
-inputfield=driver.find_elements_by_tag_name("input")
+inputfield = driver.find_elements_by_tag_name("input")
 inputfield[6].send_keys(os.getenv("pixiv_mailaddress"))
 inputfield[7].send_keys(os.getenv("pixiv_password"))
 inputfield[7].send_keys(Keys.ENTER)
 
-sleep(2)#debug
+sleep(2)
 
 for p_link in df_pixivlinks.itertuples():
-    #urlの有無
+    # urlの有無
     if p_link.url != "False":
         driver.get(p_link.url)
         sleep(2)
         try:
-            #フォローボタン探索
-            followbutton=driver.find_element_by_css_selector("button[type='submit']")
+            # フォローボタン探索
+            followbutton = driver.find_element_by_css_selector("button[type='submit']")
             followbutton.click()
             sleep(2)
         except selenium_exceptions.NoSuchElementException:
--- twpx.py
+++ twpx.py
@@ -7,65 +7,65 @@ from dotenv import load_dotenv
 import pandas as pd
 from tweepy.error import RateLimitError
 
-#正規表現
+# 正規表現
 rege_pixiv = r'.*pixiv.*'
 
-#.envロード
+# .envロード
 load_dotenv()
 consumer_key = os.getenv("API_KEY")
 consumer_secret = os.getenv("SECRET_KEY")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-#認証
+# 認証
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-#フォロー取得
+# フォロー取得
 hataraku_data = api.friends_ids("M1nPy")
 
-list_pixivlinks=[]
-#pixiv取得
-n=0
+list_pixivlinks = []
+# pixiv取得
+n = 0
 for following in hataraku_data:
-    i=0
+    i = 0
     while True:
-        #制限かかったら1分に一回try、20回で終了
+        # 制限かかったら1分に一回try、20回で終了
         try:
-            i+=1
-            status_date=api.get_user(following)
+            i += 1
+            status_date = api.get_user(following)
             break
         except RateLimitError:
-            print(str(i)+"RateLimitError")#debug
-            if i>20:
+            print(str(i)+"RateLimitError")#  debug
+            if i > 20:
                 sys.exit()
             sleep(60)
-    print(str(n)+":"+status_date.name)#debug
-    #url欄
+    print(str(n)+":"+status_date.name)#  debug
+    # url欄
     try:
-        status_url=status_date.entities["url"]["urls"][0]["expanded_url"]
+        status_url = status_date.entities["url"]["urls"][0]["expanded_url"]
         if not status_url:
-            status_url=status_date.entities["url"]["urls"][0]["url"]
+            status_url = status_date.entities["url"]["urls"][0]["url"]
             if not status_url:
-                status_url=''
+                status_url = ''
     except:
-        status_url=''
-    if re.match(rege_pixiv,status_url) != None:
-        list_pixivlinks.append((status_date.name,status_url))
-    #プロフィール欄
+        status_url = ''
+    if re.match(rege_pixiv, status_url) != None:
+        list_pixivlinks.append((status_date.name, status_url))
+    # プロフィール欄
     else:
-        status_des=status_date.entities["description"]["urls"]
+        status_des = status_date.entities["description"]["urls"]
         for des_url in status_des:
-            status_reurl=des_url["expanded_url"]
-            if re.match(rege_pixiv,status_reurl) != None:
-                list_pixivlinks.append((status_date.name,status_reurl))
+            status_reurl = des_url["expanded_url"]
+            if re.match(rege_pixiv, status_reurl) != None:
+                list_pixivlinks.append((status_date.name, status_reurl))
                 break
         else:
             list_pixivlinks.append((status_date.name,None))
     sleep(1)
-    n+=1
+    n += 1
 
-#データフレームに変換してcsv出力
-df_pixivlinks=pd.DataFrame(list_pixivlinks,columns=["name","url"]).fillna(False)
+# データフレームに変換してcsv出力
+df_pixivlinks=pd.DataFrame(list_pixivlinks, columns=["name", "url"]).fillna(False)
 df_pixivlinks.to_csv(os.getcwd()+'/pixivlink.csv')
